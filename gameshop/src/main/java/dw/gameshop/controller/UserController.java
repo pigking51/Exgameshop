9dccffd7e3a7b5ac83829abde8e238b6b3d0c937
pigking51 <145522570+pigking51@users.noreply.github.com>
2024-05-29 16:39:24 +0900

    240529final

--- gameshop/src/main/java/dw/gameshop/controller/UserController.java
+++ gameshop/src/main/java/dw/gameshop/controller/UserController.java
@@ -4,6 +4,7 @@ import dw.gameshop.dto.UserDto;
 import dw.gameshop.service.UserDetailService;
 import dw.gameshop.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -45,11 +46,23 @@ public class UserController {
         SecurityContextHolder.getContext().setAuthentication(authentication);
         // 세션 생성
         HttpSession session = request.getSession(true);
-//      // 세션에 인증 객체 저장
+        // 세션에 인증 객체 저장
         session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY,
                 SecurityContextHolder.getContext());
 
         return ResponseEntity.ok("Success");
+    }
+    @PostMapping("/logout")
+    public String logout(HttpServletRequest request,
+                         HttpServletResponse response){
+        // response는 status 200을 보내주는 주체를 의미
+        // request는 클라이언트에서 요청한 내용 원본이 들어있음
+        HttpSession session = request.getSession(false);
+        // 없으면 null값 요청하는 것
+        if(session != null){
+            session.invalidate();
+        }
+        return "You Have Been logged out!";
     }
 
     @GetMapping("current")
--- gameshop/src/main/resources/static/js/login.js
+++ gameshop/src/main/resources/static/js/login.js
@@ -1,6 +1,11 @@
 const urlLogin = "http://localhost:8080/user/login";
+const urlLogout = "http://localhost:8080/user/logout";
+const urlsignUp = "http://localhost:8080/user/signup";
+
 let userId = "";
 let password = "";
+let userName = "";
+let userEmail = "";
 
 document.querySelector("#userId").addEventListener("change", (e) => {
   console.log(e.target.value);
@@ -13,6 +18,7 @@ document.querySelector("#password").addEventListener("change", (e) => {
   console.log(e.target.value);
   password = e.target.value;
 });
+
 document.querySelector(".loginBtn").addEventListener("click", () => {
   const data = {
     userId: userId,
@@ -22,8 +28,116 @@ document.querySelector(".loginBtn").addEventListener("click", () => {
     .post(urlLogin, data, { withCredentials: true }) // url 옆에 전송할 객체 넣음
     .then((response) => {
       console.log("데이터 :", response);
+      if (response.status == 200) {
+        document.querySelector(".login-box").classList.add("hidden");
+        document.querySelector(".user-box").classList.remove("hidden");
+        document.querySelector(".user-box p").textContent =
+          response.data + "님 환영합니다.";
+      }
     })
     .catch((error) => {
       console.log("에러발생 : ", error);
     });
 });
+
+document.querySelector(".logoutBtn").addEventListener("click", () => {
+  if (confirm("로그아웃 하시겠습니까?")) {
+    axios
+      .post(urlLogout, {}, { withCredentials: true })
+      .then((response) => {
+        console.log("데이터: ", response);
+        if (response.status == 200) {
+          document.querySelector(".login-box").classList.remove("hidden");
+          document.querySelector(".user-box").classList.add("hidden");
+        }
+      })
+      .catch((error) => {
+        console.log("오류 발생: ", error);
+      });
+  }
+});
+
+document.querySelector(".goToSignUp").addEventListener("click", () => {
+  document.querySelector(".login-box").classList.add("hidden");
+  document.querySelector(".signUp-box").classList.remove("hidden");
+});
+document.querySelector(".prevBtn").addEventListener("click", () => {
+  document.querySelector(".signUp-box").classList.add("hidden");
+  document.querySelector(".login-box").classList.remove("hidden");
+});
+
+document.querySelector("#userId2").addEventListener("change", (e) => {
+  console.log(e.target.value);
+  // 확인해보면 엄청 길게 나오는데 여기서 value 값이 필요(입력한 값 나와있음)
+  //  → e.target.value 사용
+  userId = e.target.value;
+});
+
+document.querySelector("#password2").addEventListener("change", (e) => {
+  console.log(e.target.value);
+  password = e.target.value;
+});
+
+document.querySelector("#userName").addEventListener("change", (e) => {
+  console.log(e.target.value);
+  userName = e.target.value;
+});
+
+document.querySelector("#userEmail").addEventListener("change", (e) => {
+  console.log(e.target.value);
+  userEmail = e.target.value;
+});
+
+document.querySelector(".register").addEventListener("click", () => {
+  const data = {
+    userId: userId,
+    password: password,
+    userName: userName,
+    userEmail: userEmail,
+  };
+  axios
+    .post(urlsignUp, data, { withCredentials: true }) // url 옆에 전송할 객체 넣음
+    .then((response) => {
+      console.log("데이터 :", response);
+      if (data.userId == response.data.userId) {
+        console.log("중복된 아이디입니다.");
+      } else if (data.userEmail == response.data.userEmail) {
+        console.log("이미 가입한 이력이 있습니다.");
+      } else if (data.userName == response.data.userName) {
+        console.log("이미 가입한 이력이 있습니다.");
+      }
+      if (response.status == 201) {
+        console.log("회원가입 완료");
+        document.querySelector("#userId2").value = null;
+        document.querySelector("#password2").value = null;
+        document.querySelector("#userName").value = null;
+        document.querySelector("#userEmail").value = null;
+        document.querySelector(".signUp-box").classList.add("hidden");
+        document.querySelector(".login-box").classList.remove("hidden");
+      }
+    })
+    .catch((error) => {
+      console.log("에러발생 : ", error);
+    });
+});
+
+function sessionCurrent() {
+  axios
+    .get("http://localhost:8080/user/current", { withCredentials: true })
+    .then((response) => {
+      console.log("데이터: ", response);
+      if (response.status == 200) {
+        console.log("세션 유지");
+        document.querySelector(".login-box").classList.add("hidden");
+        document.querySelector(".user-box").classList.remove("hidden");
+        document.querySelector(".user-box p").textContent =
+          response.data + "님 환영합니다.";
+      }
+    })
+    .catch((error) => {
+      console.log("에러 발생: ", error);
+    });
+}
+
+// js 파일이 로드될 때 호출됨
+sessionCurrent();
